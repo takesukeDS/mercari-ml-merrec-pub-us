d73fca3bb22b7bd8843dc7d7d68fccc00e3effce
takesuke <hoge>
2025-07-14 07:00:09 +0900

    add custom tokens to tokenizer

--- mercatran/main_embed.py
+++ mercatran/main_embed.py
@@ -4,6 +4,7 @@ import pathlib
 import sys
 from argparse import ArgumentParser
 from functools import partial
+import pandas as pd
 
 import config
 import torch
@@ -14,7 +15,7 @@ from data import (
     collate_batch_item,
     collate_batch_item_val,
     sequence_dataset,
-    train_tokenizer,
+    train_tokenizer, collate_batch_item_wrapper, collate_batch_item_val_wrapper,
 )
 from embed import (
     ItemEmbeddings,
@@ -39,115 +40,69 @@ from sklearn.model_selection import train_test_split
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
-from pandas import DataFrame as pd
 
 
-EVENT_ID_TO_TOKEN = {
-    "item_view": "<item_view>",
-    "item_like":  "<item_like>",
-    "item_add_to_cart_tap":  "<item_add_to_cart_tap>",
-    "offer_make":  "<offer_make>",
-    "buy_start":  "<buy_start>",
-    "buy_comp":  "<buy_comp>",
-}
+class UserEncoderEmbeddings(nn.Module):
+    def __init__(self, padding_idx, use_event_id=False):
+        super(UserEncoderEmbeddings, self).__init__()
+        self.user_embed = UserEmbeddings(
+            vocab_size=config.BPE_VOCAB_LIMIT,
+            d_model=config.D_MODEL,
+            padding_idx=padding_idx,
+            max_norm=config.MAX_NORM,
+            use_event_id=use_event_id,
+        )
+        self.unflatten = nn.Unflatten(0, (config.BATCH_SIZE, -1))
+        self.pos_embed = PositionalEncoding(config.D_MODEL, config.DROPOUT, config.POSITION_MAX_LEN)
 
-SHIPPER_ID_TO_TOKEN = {
-    1: "<Buyer>",
-    2: "<Seller>"
-}
-
-def preprocess_before_training_tokenizer(seq_dataset, args):
-    if args.all_categories:
-        # replace sharp sign with a space
-        seq_dataset['category_name'] = seq_dataset['category_name'].str.replace('#', ' ', regex=False)
-    else:
-        seq_dataset['category_name'] = seq_dataset['category_name'].map(
-            lambda x: x.split('#')[0] or x.split('#')[1] or x.split('#')[2])
-
-def combine_tokens(tokens, trim=True):
-    if isinstance(tokens, pd.Series):
-        tokens = tokens.tolist()
-    if trim:
-        # Remove the first and last characters (e.g., '<' and '>')
-        tokens = [token[1:-1] for token in tokens]
-    combined = ','.join(tokens)
-    return "<" + combined + ">"
-
-def preprocess_after_training_tokenizer(seq_dataset, tokenizer, args):
-    # add special tokens to category_name
-    num_added = 0
-    if args.add_event_id:
-        seq_dataset["event_id"] = seq_dataset["event_id"].map(EVENT_ID_TO_TOKEN)
-        # raise exception if event_id contains nan
-        if seq_dataset["event_id"].isnull().any():
-            raise ValueError("event_id contains NaN values. Please check your data.")
-        seq_dataset['category_name'] = seq_dataset['category_name'] + seq_dataset["event_id"]
-        num_added += tokenizer.add_special_tokens(list(EVENT_ID_TO_TOKEN.values()))
-
-    if args.add_shipper_id:
-        seq_dataset["shipper_id"] = seq_dataset["shipper_id"].map(SHIPPER_ID_TO_TOKEN)
-        # raise exception if shipper_id contains nan
-        if seq_dataset["shipper_id"].isnull().any():
-            raise ValueError("shipper_id contains NaN values. Please check your data.")
-        seq_dataset['category_name'] = seq_dataset['category_name'] + seq_dataset["shipper_id"]
-        num_added += tokenizer.add_special_tokens(list(SHIPPER_ID_TO_TOKEN.values()))
-
-    if args.add_event_id and args.add_shipper_id:
-        seq_dataset['tmp'] = seq_dataset[['event_id', 'shipper_id']].apply(
-            combine_tokens, axis=1)
-        seq_dataset['category_name'] = seq_dataset['category_name'] + seq_dataset['tmp']
-        combined_tokens = []
-        for event_id in EVENT_ID_TO_TOKEN.values():
-            for shipper_id in SHIPPER_ID_TO_TOKEN.values():
-                combined_tokens.append(combine_tokens([event_id, shipper_id]))
-        num_added += tokenizer.add_special_tokens(combined_tokens)
-    return num_added
+    def forward(self, x, event_id=None):
+        x = self.user_embed(x, event_id)
+        x = self.unflatten(x)
+        x = self.pos_embed(x)
+        return x
 
 
 def main(args):
     model_dir = pathlib.Path(args.save_path)
     model_dir.mkdir(parents=True, exist_ok=True)
     data_path = args.data_path
-    data_path = args.data_path
     data = pd.read_pickle(data_path)
     seq_dataset = pd.DataFrame.from_dict(data, orient='index')
-    # seq_dataset = seq_dataset[
-    #     ['seq_user_id', 'name', 'category_name', 'brand_name', 'category_id', 'brand_id', 'item_id', 'event_id']]
-    preprocess_before_training_tokenizer(seq_dataset, args)
-    logging.info(seq_dataset.iloc[0])
+    seq_dataset = seq_dataset[
+        ['seq_user_id', 'name', 'category_name', 'brand_name', 'category_id', 'brand_id', 'item_id', 'event_id']]
     tokenizer = train_tokenizer(df_train=seq_dataset)
-    num_added_tokens = preprocess_after_training_tokenizer(seq_dataset, tokenizer, args)
     tokenizer.save(os.path.join(args.save_path, args.tokenizer_save_name))
-    logging.info(seq_dataset.iloc[0])
-    logging.info(f"Number of added tokens: {num_added_tokens}, New tokenizer size: {tokenizer.get_vocab_size()}")
     train_df, test_df = train_test_split(
         seq_dataset, test_size=args.test_frac, random_state=args.seed)
     val_df, test_df = train_test_split(
         test_df, test_size=0.5, random_state=args.seed)
 
-    train_dataset = UserItemInteractionDataset(interactions=train_df)
-    val_dataset = UserItemInteractionDataset(interactions=val_df)
-    test_dataset = UserItemInteractionDataset(interactions=test_df)
+    train_dataset = UserItemInteractionDataset(interactions=train_df, return_event_id=args.use_event_id)
+    val_dataset = UserItemInteractionDataset(interactions=val_df, return_event_id=args.use_event_id)
+    test_dataset = UserItemInteractionDataset(interactions=test_df, return_event_id=args.use_event_id)
 
     train_loader = DataLoader(
         train_dataset,
         batch_size=config.BATCH_SIZE,
         shuffle=True,
-        collate_fn=partial(collate_batch_item, tokenizer=tokenizer),
+        collate_fn=partial(collate_batch_item_wrapper if args.use_event_id else collate_batch_item,
+                           tokenizer=tokenizer),
         drop_last=True,
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=config.BATCH_SIZE,
         shuffle=False,
-        collate_fn=partial(collate_batch_item_val, tokenizer=tokenizer),
+        collate_fn=partial(collate_batch_item_val_wrapper if args.use_event_id else collate_batch_item_val,
+                           tokenizer=tokenizer),
         drop_last=True,
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=config.BATCH_SIZE,
         shuffle=False,
-        collate_fn=partial(collate_batch_item_val, tokenizer=tokenizer),
+        collate_fn=partial(collate_batch_item_val_wrapper if args.use_event_id else collate_batch_item_val,
+                           tokenizer=tokenizer),
         drop_last=True,
     )
 
@@ -162,16 +117,9 @@ def main(args):
             ),
             config.NUM_STACKS,
         ),
-        nn.Sequential(  # user_encoder_embed
-            UserEmbeddings(
-                vocab_size=config.BPE_VOCAB_LIMIT + num_added_tokens,
-                d_model=config.D_MODEL,
-                padding_idx=tokenizer.token_to_id(MASK_TOKEN),
-                max_norm=config.MAX_NORM,
-            ),
-            nn.Unflatten(0, (config.BATCH_SIZE, -1)),
-            PositionalEncoding(config.D_MODEL, config.DROPOUT,
-                               config.POSITION_MAX_LEN),
+        UserEncoderEmbeddings(
+            padding_idx=tokenizer.token_to_id(MASK_TOKEN),
+            use_event_id=args.use_event_id,
         ),
         Decoder(  # user_decoder
             DecoderLayer(
@@ -186,7 +134,7 @@ def main(args):
         ),
         nn.Sequential(  # user_decoder_embed
             ItemEmbeddings(
-                vocab_size=config.BPE_VOCAB_LIMIT + num_added_tokens,
+                vocab_size=config.BPE_VOCAB_LIMIT,
                 d_model=config.D_MODEL,
                 padding_idx=tokenizer.token_to_id(MASK_TOKEN),
                 max_norm=config.MAX_NORM,
@@ -207,7 +155,7 @@ def main(args):
         ),
         nn.Sequential(  # item_encoder_embed
             ItemEmbeddings(
-                vocab_size=config.BPE_VOCAB_LIMIT + num_added_tokens,
+                vocab_size=config.BPE_VOCAB_LIMIT,
                 d_model=config.D_MODEL,
                 padding_idx=tokenizer.token_to_id(MASK_TOKEN),
                 max_norm=config.MAX_NORM,
@@ -243,7 +191,12 @@ def main(args):
         for i, batch in enumerate(
             tqdm(train_loader, desc=f"Epoch[{epoch}] Training batch")
         ):
-            user, user_mask, item, item_mask, item_y, item_mask_y = batch
+            if args.use_event_id:
+                # If using event_id, the batch will have an additional element
+                user, user_mask, item, item_mask, item_y, item_mask_y, event_id = batch
+            else:
+                user, user_mask, item, item_mask, item_y, item_mask_y = batch
+                event_id = None
             user_embed, item_embed = model(
                 user,
                 user_mask.unsqueeze(-2),
@@ -251,6 +204,7 @@ def main(args):
                 create_user_target_mask(item_mask.unsqueeze(-2)),
                 item_y,
                 create_item_encoder_mask(item_mask_y.unsqueeze(-2)),
+                user_event_id=event_id,
             )
             target = torch.arange(user_embed.size(
                 0) * user_embed.size(1)).to(config.DEVICE)
@@ -288,6 +242,7 @@ def main(args):
                     eval_ks=config.EVAL_Ks,
                     tokenizer=tokenizer,
                     out_dir=args.metrics_path,
+                    use_event_id=args.use_event_id,
                 )
                 _ = critic.evaluate(epoch_train=epoch, desc="val")
                 del critic
@@ -305,6 +260,7 @@ def main(args):
             eval_ks=config.EVAL_Ks,
             tokenizer=tokenizer,
             out_dir=args.metrics_path,
+            use_event_id=args.use_event_id,
         )
         _ = critic.evaluate(epoch_train=epoch, desc="final_test")
 
@@ -321,11 +277,8 @@ def parse_args(description):
     parser.add_argument("--max_len", type=int, default=22)
     parser.add_argument("--min_seq_len", type=int, default=5)
     parser.add_argument("--test_frac", type=int, default=0.3)
-    parser.add_argument("--add_event_id", action='store_true',
-                        help="Add event_id to category_name")
-    parser.add_argument("--add_shipper_id", action='store_true',
-                        help="Add shipper_id to category_name")
-    parser.add_argument("--all_categories", action='store_true',)
+    parser.add_argument("--use_event_id", action='store_true',
+                        help="Use event_id in the dataset")
     args = parser.parse_args()
     return args
 
